baa8c620f752dfa3a26aa13d990086fe09040228
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-12-16 21:54:33 +0530

    Refactor App component to use class state

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,96 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-            const [name1, setName1] = useState("");
-    const [name2, setName2] = useState("");
-    const [flameResult, setFlameResult] = useState("");
-  function flamesLogic(n1,n2){
+  constructor(props) {
+    super(props);
+    this.state = {
+      name1: "",
+      name2: "",
+      flameResult: ""
+    };
+  }
+
+  flamesLogic = (n1, n2) => {
     if (n1 === "" || n2 === "") {
       return "Please Enter valid input";
     }
-    let flamesArr=["Siblings","Friend","Love","Affection","Marriage","Enemy"]
-    
-    let a1=n1.split("");
-    let a2=n2.split("");
-    for(let i=0;i<a1.length;i++){
-        let currIndx=a2.indexOf(a1[i])
-        if(currIndx!=-1){
-          a1[i]="";
-          a2[currIndx]="";
-        }
-    }
-    let size=a1.filter((ch)=>ch!="").length+a2.filter((ch)=>ch!="").length
-    return flamesArr[size%6];
-  }
-        return(
-            <div id="main">
-                           <h3>Flames:</h3>
-            
-            <form onSubmit={(e) => {
-                e.preventDefault();
-                let resultLogic = flamesLogic(name1, name2);
-                setFlameResult(resultLogic)
-            }}>
-                <input type='text' placeholder='name1' onChange={(e) => setName1(e.target.value)} />
-                <input type='text' placeholder='name2' onChange={(e) => setName2(e.target.value)} />
-                <input type='submit' value='calculate_relationship' />
-            </form>
-            <button onClick={()=>{
-              setName1("")
-              setName2("")
-              setFlameResult("")
-            }
-            }>clear</button>
-            <h3>{flameResult}</h3>
-            </div>
-        )
-    }
-}
 
+    const flamesArr = [
+      "Siblings",   // 0
+      "Friends",    // 1
+      "Love",       // 2
+      "Affection",  // 3
+      "Marriage",   // 4
+      "Enemy"       // 5
+    ];
+
+    let a1 = n1.split("");
+    let a2 = n2.split("");
+
+    for (let i = 0; i < a1.length; i++) {
+      const index = a2.indexOf(a1[i]);
+      if (index !== -1) {
+        a1[i] = "";
+        a2[index] = "";
+      }
+    }
+
+    const size =
+      a1.filter((c) => c !== "").length +
+      a2.filter((c) => c !== "").length;
+
+    return flamesArr[size % 6];
+  };
+
+  render() {
+    return (
+      <div id="main">
+        <input
+          type="text"
+          name="name1"
+          data-testid="input1"
+          value={this.state.name1}
+          onChange={(e) => this.setState({ name1: e.target.value })}
+        />
+
+        <input
+          type="text"
+          name="name2"
+          data-testid="input2"
+          value={this.state.name2}
+          onChange={(e) => this.setState({ name2: e.target.value })}
+        />
+
+        <button
+          name="calculate_relationship"
+          data-testid="calculate_relationship"
+          onClick={() =>
+            this.setState({
+              flameResult: this.flamesLogic(
+                this.state.name1,
+                this.state.name2
+              )
+            })
+          }
+        >
+          calculate_relationship
+        </button>
+
+        <button
+          name="clear"
+          data-testid="clear"
+          onClick={() =>
+            this.setState({ name1: "", name2: "", flameResult: "" })
+          }
+        >
+          clear
+        </button>
+
+        <h3 data-testid="answer">{this.state.flameResult}</h3>
+      </div>
+    );
+  }
+}
 
 export default App;
